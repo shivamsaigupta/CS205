391eec82ba5c656a391641471d704ea0bbca3760
shivamsaigupta <contact@shivam.me>
2016-10-30 23:59:42 +0530

    Q1 & Q3

---
+++ Homeworks/HW3/Q3.scala
@@ -0,0 +1,38 @@
+class Queue[T] {
+  
+	var lst: List[Any]=Nil
+	
+	def equals(that: Queue[T]): Boolean={
+		(this.lst, that.lst) match{
+		  case _ => false	
+		  case (Nil, Nil) => true
+			case (hd1::rest1, hd2::rest2) => (hd1.equals(hd2)) && (rest1.equals(rest2))
+			
+		}
+	}
+	override def hashCode(): Int={
+		var sum: Int=0
+		this.el.foreach{
+			sum+=_.hashCode()
+		}
+		sum
+	}
+	def Enqueue(x: Any): Unit={
+		lst = x :: lst
+		print(lst)
+	}
+	def Dequeue(): Unit={
+		lst match{
+			case Nil => lst=Nil
+			case _ => lst=lst.init
+		}
+		println(lst)
+	}
+}
+
+object Q1{
+  def main(args:Array[String]):Unit = {
+    var a = new Queue()
+    a.enqueue(5);
+  }
+}
